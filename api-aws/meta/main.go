8b98c9f598d99928a941b84ea90f44c0fc2ea304
IggyBlob <socialmedia@paulhaunschmied.com>
2018-07-31 16:33:13 +0200

    Apply enhanced JSON structure to function meta

--- api-aws/meta/main.go
+++ api-aws/meta/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"github.com/RadioCheckerApp/api/api-aws/awsutil"
+	"github.com/RadioCheckerApp/api/model"
 	"github.com/RadioCheckerApp/api/shared"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func Handler() (events.APIGatewayProxyResponse, error) {
-	return events.APIGatewayProxyResponse{
-		Body:       shared.APIMetadata(),
-		StatusCode: 200,
-	}, nil
+	responseMessage := model.NewAPIResponseMessage(shared.APIMetadata(), nil)
+	return awsutil.CreateResponse(200, responseMessage), nil
 }
 
 func main() {
