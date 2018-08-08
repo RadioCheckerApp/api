8c8d67fdc9fa514fdc9d218bedb9086389605554
IggyBlob <socialmedia@paulhaunschmied.com>
2018-08-08 20:37:17 +0200

    Apply refactored architecture to function tracks

--- api-aws/tracks/main.go
+++ api-aws/tracks/main.go
@@ -4,7 +4,7 @@ import (
 	"github.com/RadioCheckerApp/api/api-aws/awsutil"
 	"github.com/RadioCheckerApp/api/datalayer"
 	"github.com/RadioCheckerApp/api/model"
-	"github.com/RadioCheckerApp/api/shared"
+	"github.com/RadioCheckerApp/api/request"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
@@ -13,15 +13,21 @@ import (
 	"os"
 )
 
-func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func Handler(apiRequest events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// AWS config implicitly defined by serverless.yml
 	dbSession, _ := session.NewSession(&aws.Config{})
 
 	db := dynamodb.New(dbSession)
 	trackRecordsDAO := datalayer.NewDDBTrackRecordDAO(db, os.Getenv("TRACKRECORDS_TABLE"))
 
-	tracks, err := shared.Tracks(trackRecordsDAO, request.PathParameters,
-		request.QueryStringParameters)
+	worker, err := request.CreateTracksWorker(trackRecordsDAO, apiRequest.PathParameters,
+		apiRequest.QueryStringParameters)
+	if err != nil {
+		responseMessage := model.NewAPIResponseMessage(nil, err)
+		return awsutil.CreateResponse(200, responseMessage), nil
+	}
+
+	tracks, err := worker.HandleRequest()
 	responseMessage := model.NewAPIResponseMessage(tracks, err)
 	return awsutil.CreateResponse(200, responseMessage), nil
 }
