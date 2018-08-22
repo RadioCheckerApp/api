bc59c7aa6b744e49f4cffd5c515329fe2ae3420b
IggyBlob <socialmedia@paulhaunschmied.com>
2018-08-22 21:53:51 +0200

    Implement function tracks-create

---
+++ api-aws/tracks-create/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/RadioCheckerApp/api/api-aws/awsutil"
+	"github.com/RadioCheckerApp/api/datalayer"
+	"github.com/RadioCheckerApp/api/model"
+	"github.com/RadioCheckerApp/api/request"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"os"
+)
+
+func Handler(apiRequest events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// AWS config implicitly defined by serverless.yml
+	dbSession, _ := session.NewSession(&aws.Config{})
+
+	db := dynamodb.New(dbSession)
+	trackRecordsDAO := datalayer.NewDDBTrackRecordDAO(
+		db,
+		os.Getenv("TRACKRECORDS_TABLE"),
+		os.Getenv("TRACKRECORDS_TABLE_GSI_TYPE_AIRTIME"),
+	)
+	stationDAO := datalayer.NewDDBStationDAO(
+		db,
+		os.Getenv("STATIONS_TABLE"),
+	)
+
+	worker, err := request.CreateCreateTrackWorker(
+		trackRecordsDAO,
+		stationDAO,
+		apiRequest.PathParameters,
+		[]byte(apiRequest.Body),
+	)
+	if err != nil {
+		responseMessage := model.NewAPIResponseMessage(nil, err)
+		return awsutil.CreateResponse(200, responseMessage), nil
+	}
+
+	tracks, err := worker.HandleRequest()
+	responseMessage := model.NewAPIResponseMessage(tracks, err)
+	return awsutil.CreateResponse(200, responseMessage), nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
